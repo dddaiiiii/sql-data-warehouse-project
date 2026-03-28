7235bc16a91fcebe645fe926b05e76e1705eae79
rolaatarek <rola222101415@nmu.edu.eg>
2026-03-28 10:56:44 +0200

    Create ddl_silver.sql
    This script creates three tables in the silver layer: diabetes, heart, and hypertension, each with various attributes related to health metrics.

---
+++ silver/ddl_silver.sql
@@ -0,0 +1,97 @@
+/*
+DDL Script: Create Silver Tables
+*/
+
+-- =============================================
+-- 1. DIABETES
+-- =============================================
+IF OBJECT_ID('silver.diabetes', 'U') IS NOT NULL
+    DROP TABLE silver.diabetes;
+GO
+
+CREATE TABLE silver.diabetes (
+    diabetes_id         INT IDENTITY(1,1) PRIMARY KEY,
+    gender              VARCHAR(20),
+    age                 FLOAT,
+    hypertension        INT,
+    heart_disease       INT,
+    smoking_history     VARCHAR(30),
+    bmi                 FLOAT,
+    hba1c_level         FLOAT,
+    blood_glucose_level INT,
+    diabetes            INT,
+    disease_category    VARCHAR(20)
+);
+GO
+
+
+-- =============================================
+-- 2. HEART
+-- =============================================
+IF OBJECT_ID('silver.heart', 'U') IS NOT NULL
+    DROP TABLE silver.heart;
+GO
+
+CREATE TABLE silver.heart (
+    heart_id             INT IDENTITY(1,1) PRIMARY KEY,
+    gender               VARCHAR(10),
+    age                  FLOAT,
+    blood_pressure       FLOAT,
+    cholesterol_level    FLOAT,
+    exercise_habits      VARCHAR(20),
+    smoking              VARCHAR(10),
+    family_heart_disease VARCHAR(10),
+    diabetes             VARCHAR(10),
+    bmi                  FLOAT,
+    high_blood_pressure  VARCHAR(10),
+    low_hdl_cholesterol  VARCHAR(10),
+    high_ldl_cholesterol VARCHAR(10),
+    alcohol_consumption  VARCHAR(20),
+    stress_level         VARCHAR(20),
+    sleep_hours          FLOAT,
+    sugar_consumption    VARCHAR(20),
+    triglyceride_level   FLOAT,
+    fasting_blood_sugar  FLOAT,
+    crp_level            FLOAT,
+    homocysteine_level   FLOAT,
+    heart_disease_status VARCHAR(10),
+    disease_category     VARCHAR(20)
+);
+GO
+
+
+-- =============================================
+-- 3. HYPERTENSION
+-- =============================================
+IF OBJECT_ID('silver.hypertension', 'U') IS NOT NULL
+    DROP TABLE silver.hypertension;
+GO
+
+CREATE TABLE silver.hypertension (
+    hypertension_id         INT IDENTITY(1,1) PRIMARY KEY,
+    country                 VARCHAR(50),
+    age                     INT,
+    bmi                     FLOAT,
+    cholesterol             INT,
+    systolic_bp             INT,
+    diastolic_bp            INT,
+    smoking_status          VARCHAR(20),
+    alcohol_intake          FLOAT,
+    physical_activity_level VARCHAR(20),
+    family_history          VARCHAR(10),
+    diabetes                VARCHAR(10),
+    stress_level            INT,
+    salt_intake             FLOAT,
+    sleep_duration          FLOAT,
+    heart_rate              INT,
+    ldl                     INT,
+    hdl                     INT,
+    triglycerides           INT,
+    glucose                 INT,
+    gender                  VARCHAR(10),
+    education_level         VARCHAR(20),
+    employment_status       VARCHAR(20),
+    hypertension            VARCHAR(10),
+    disease_category        VARCHAR(20)
+);
+GO
